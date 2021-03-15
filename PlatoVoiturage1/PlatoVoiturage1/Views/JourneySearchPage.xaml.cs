20dc90f11e54a4999693ec71fa1eccc7a5961b20
Red2Je <63746333+Red2Je@users.noreply.github.com>
2021-03-15 23:11:10 +0100

    Inteface fonctionnelle sans base de données

--- PlatoVoiturage1/PlatoVoiturage1/Views/JourneySearchPage.xaml.cs
+++ PlatoVoiturage1/PlatoVoiturage1/Views/JourneySearchPage.xaml.cs
@@ -10,32 +10,18 @@ using Xamarin.Forms.Xaml;
 namespace PlatoVoiturage1.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class HomePage : ContentPage
+    public partial class JourneySearchPage : ContentPage
     {
-
-        private List<int> journey = new List<int>();
-        
-        public HomePage()
+        public JourneySearchPage()
         {
             InitializeComponent();
-
-            for(int i = 0; i<10; i++)
-            {
-                journey.Add(i);
-            }
         }
-
         private void ImageButton_Clicked(object sender, EventArgs e)
         {
             if (!Shell.Current.FlyoutIsPresented)
             {
                 Shell.Current.FlyoutIsPresented = true;
             }
-        }
-
-        private async void GoToLoginPage(object sender, EventArgs e)
-        {
-            await Shell.Current.Navigation.PushAsync(new LoginPage());
         }
     }
 }
