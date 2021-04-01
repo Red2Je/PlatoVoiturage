d546c555920e2ab52c3c0688f5627432b9ae24ed
Red2Je <63746333+Red2Je@users.noreply.github.com>
2021-04-01 15:35:38 +0200

    Test database

--- PlatoVoiturage1/PlatoVoiturage1/Views/HomePage.xaml.cs
+++ PlatoVoiturage1/PlatoVoiturage1/Views/HomePage.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Npgsql;
+
 namespace PlatoVoiturage1.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -23,6 +25,46 @@ namespace PlatoVoiturage1.Views
             {
                 journey.Add(i);
             }
+
+            //DataBase test
+           
+        }
+
+
+        private async Task connectToDatabase()
+        {
+            var cs = "Host=192.168.1.139;Username=postgres;Password=platovoiturage;Database=postgres";
+
+            try
+            {
+                var con = new NpgsqlConnection(cs);
+                await con.OpenAsync();
+                if (con.State == System.Data.ConnectionState.Open)
+                {
+                    Console.WriteLine("Connection open");
+                    var command = con.CreateCommand();
+                    command.CommandText = "SELECT * FROM trajet;";
+                    try
+                    {
+                        var reader = command.ExecuteReaderAsync();
+
+                        con.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("plouf2");
+                        Console.WriteLine(e.ToString());
+                    }
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("plouf1");
+                Console.WriteLine(e.ToString());
+            }
+
         }
 
         private void ImageButton_Clicked(object sender, EventArgs e)
