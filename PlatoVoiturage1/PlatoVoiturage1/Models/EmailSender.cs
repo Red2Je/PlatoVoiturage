8ffb30b60d8badfb9af16df60ce6449b96d60ef9
Red2Je <63746333+Red2Je@users.noreply.github.com>
2021-04-23 16:26:25 +0200

    EmailSender

---
+++ PlatoVoiturage1/PlatoVoiturage1/Models/EmailSender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace PlatoVoiturage1.Models
+{
+    public static class EmailSender
+    {
+        public static string Email { get; set; }
+        public static ContentPage HomePage { get; set; };
+
+    }
+}
