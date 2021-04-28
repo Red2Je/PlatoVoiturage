2f7770785bbd26ff3391980fa2ef4f67732a8159
IstAviX <istavix@gmail.com>
2021-04-28 17:45:00 +0200

    ProposerTrajet milliseconds fix

--- PlatoVoiturage1/PlatoVoiturage1/Views/ProposerTrajet.xaml.cs
+++ PlatoVoiturage1/PlatoVoiturage1/Views/ProposerTrajet.xaml.cs
@@ -18,41 +18,16 @@ namespace PlatoVoiturage1.Views
         public ProposerTrajet()
         {
             InitializeComponent();
-            DepartureTime.Time = DateTime.Now.TimeOfDay;
+            TimeSpan initTime = new TimeSpan(DateTime.Now.TimeOfDay.Hours, DateTime.Now.TimeOfDay.Minutes, 0);
+            DepartureTime.Time = initTime;
             DepartureDate.MinimumDate = DateTime.Now.Date;
-            ArrTime.Time = DateTime.Now.TimeOfDay;
+            ArrTime.Time = initTime;
             ArrDate.MinimumDate = DepartureDate.Date;
-            Console.WriteLine(DepartureTime.Time.Seconds);
+            Console.WriteLine(DepartureTime.Time);
         }
 
         private async void Valider(object sender, EventArgs e)
         {
-            
-            /*
-            string dateDep = DepartureDate.Date.Year.ToString() + "-";
-            if(DepartureDate.Date.Month < 10)
-            {
-                dateDep += "0";
-            }
-            dateDep += DepartureDate.Date.Month.ToString() + "-";
-            if (DepartureDate.Date.Day < 10)
-            {
-                dateDep += "0";
-            }
-            dateDep += DepartureDate.Date.Day.ToString() + " ";
-
-            string dateArr = ArrDate.Date.Year.ToString() + "-";
-            if (ArrDate.Date.Month < 10)
-            {
-                dateArr += "0";
-            }
-            dateArr += ArrDate.Date.Month.ToString() + "-";
-            if (ArrDate.Date.Day < 10)
-            {
-                dateArr += "0";
-            }
-            dateArr += ArrDate.Date.Day.ToString() + " ";*/
-
 
             int passengers = 0;
             try
@@ -74,7 +49,7 @@ namespace PlatoVoiturage1.Views
                 km = 0;
             }
  
-            Journey j = new Journey(0, depAd.Text, depVil.Text, arrAd.Text, arrVil.Text, DepartureDate.Date.ToString("yyyy-MM-dd") +" "+ DepartureTime.Time.ToString("HH:mm:ss"), ArrDate.Date.ToString("yyyy-MM-dd") +" "+ ArrTime.Time.ToString("HH:mm:ss"), km, passengers, comm.Text, dog.BackgroundColor == Color.Green, smoke.BackgroundColor == Color.Green, music.BackgroundColor == Color.Green, talk.BackgroundColor == Color.Green);
+            Journey j = new Journey(0, depAd.Text, depVil.Text, arrAd.Text, arrVil.Text, DepartureDate.Date.ToString("yyyy-MM-dd") +" "+ DepartureTime.Time.ToString(), ArrDate.Date.ToString("yyyy-MM-dd") +" "+ ArrTime.Time.ToString(), km, passengers, comm.Text, dog.BackgroundColor == Color.Green, smoke.BackgroundColor == Color.Green, music.BackgroundColor == Color.Green, talk.BackgroundColor == Color.Green);
             
             
             try
